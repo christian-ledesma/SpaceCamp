bf83a217accbd9f970eea66292809b1d01384afb
Christian Ledesma <christian.ledesma@copernicus.es>
2023-03-09 13:50:18 +0100

    Add photo entity and relations

--- SpaceCamp.Persistence/Data/SpaceCampContext.cs
+++ SpaceCamp.Persistence/Data/SpaceCampContext.cs
@@ -13,6 +13,7 @@ namespace SpaceCamp.Persistence.Data
 
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
