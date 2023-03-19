dfba1876f337729d26358513f3bf071d766adc9a
Christian Ledesma <christian.ledesma@copernicus.es>
2023-03-19 02:21:44 +0100

    pt 2

---
+++ SpaceCamp.Application/Features/Activities/ActivityParams.cs
@@ -0,0 +1,12 @@
+﻿using SpaceCamp.Application.Core;
+using System;
+
+namespace SpaceCamp.Application.Features.Activities
+{
+    public class ActivityParams : PagingParams
+    {
+        public bool IsGoing { get; set; }
+        public bool IsHost { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    }
+}
